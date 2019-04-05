e0bf76538d62db9c5a829ff9d7bf79b918117fef
augustoaccorsi <augusto.acorsi@gmail.com>
2019-04-05 19:52:40 -0300

    create node class and buildNode method

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
+import re
+from Node import Node
+
+nodeList = []
+
+def buildNode(line):
+    line = line.split()
+    node = Node(int(line[0]), int(line[1]), int(line[2]))
+    nodeList.append(node)
+
 def readFile():
     with open("instancias/TSP/berlin52.tsp.txt", 'r') as f:
         for line in f:
-            print(line)
+            buildNode(line)
 
 readFile()
---
+++ Node.py
@@ -0,0 +1,9 @@
+class Node:
+    id = 0
+    x = 0
+    y = 0
+
+    def __init__(self, id, x, y):
+        self.id = id
+        self.x = x
+        self.y = y
