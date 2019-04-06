a466dc1b93ce3ce7a0e09b367ddecf86d299c4d8
Arthur TC <arthurtcabral@gmail.com>
2019-04-06 20:29:36 -0300

    Subir alterações iniciais de calculo.

--- Point.py
+++ Point.py
@@ -7,3 +7,7 @@ class Point:
         self.id = id
         self.x = x
         self.y = y
+
+    def calculateDistance(Point a, Point b):
+        distance = (((a.x - b.x) ** 2) + ((a.y - b.y) ** 2)) ** 0.5
+        return distance
